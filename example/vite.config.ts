bf4f7d24e5463fec5eb7af89f09086cb8f8e51db
Benjamin VIELLARD <gbicou@gmail.com>
2023-01-04 15:18:05 +0100

    example use package name

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import viteImageInfo from "../src/index";
+import viteImageInfo from "vite-image-info";
 
 // https://vitejs.dev/config/
 export default defineConfig({
